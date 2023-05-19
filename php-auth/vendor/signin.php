5a12f4f243dad446cf2902cb3eefa3f9746747d8
2Metallum <k.akshentsev04@mail.ru>
2023-05-19 20:47:49 +0500

    signin correct

--- php-auth/vendor/signin.php
+++ php-auth/vendor/signin.php
@@ -5,16 +5,14 @@
     $login = $_POST['login'];
     $password = md5($_POST['password']);
 
-    $stmt = mysqli_prepare($connect, "SELECT
-                                                    * 
-                                                FROM
-                                                    users
-                                                WHERE 
-                                                    `login` = ? AND `password` = ?");
-    mysqli_stmt_bind_param($stmt, "ss", $login, $password);
-    mysqli_stmt_execute($stmt);
-    if( mysqli_num_rows($stmt) > 0) {
-        $user = mysqli_fetch_assoc($stmt);
+    $check_user = mysqli_query($connect, "SELECT
+                                                        * 
+                                                    FROM
+                                                        users
+                                                    WHERE 
+                                                        `login` = '$login' AND `password` = '$password'");
+    if( mysqli_num_rows($check_user) > 0) {
+        $user = mysqli_fetch_assoc($check_user);
 
         $_SESSION['user'] = [
             "name" => $user['name'],
@@ -23,11 +21,9 @@
             "avatar" => $user['avatar']
         ];
 
-        mysqli_stmt_close($stmt);
-        mysqli_close($connect);
         header('Location: ../profile.php');
     } else {
         $_SESSION['message'] = 'Неверный логин или пароль';
         header('Location: ../index.php');
     }
-    ?>
+?>
